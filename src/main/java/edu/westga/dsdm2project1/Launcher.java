96a792163351e43c4485fdf26c4f0e03c7e5bf1a
NSamples18 <ns00184@my.westga.edu>
2026-09-09 13:33:02 -0400

    Cleaned up project structure and connected fxml to the view.

--- src/main/java/edu/westga/dsdm2project1/Launcher.java
+++ src/main/java/edu/westga/dsdm2project1/Launcher.java
@@ -1,10 +1,9 @@
 package edu.westga.dsdm2project1;
 
-import edu.westga.dsdm2project1.View.HelloApplication;
 import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
+        Application.launch(Main.class, args);
     }
 }
---
+++ src/main/java/edu/westga/dsdm2project1/Main.java
@@ -0,0 +1,29 @@
+package edu.westga.dsdm2project1;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Entry point for the Hex N-Queens JavaFX application.
+ */
+public class Main extends Application {
+
+    private static final String VIEW_PATH = "/edu/westga/dsdm2project1/Hexboard.fxml";
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource(VIEW_PATH));
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("Hex N-Queens");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
