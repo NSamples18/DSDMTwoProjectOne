00c2bf2d77f7bd18a301a5f6aed0aa7ba1d616fd
NSamples18 <ns00184@my.westga.edu>
2026-09-09 12:43:47 -0400

    Fix module-info so JUnit can access test packages

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,12 @@
 module edu.westga.dsdm2project {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires org.junit.jupiter.api;
 
     opens edu.westga.dsdm2project1 to javafx.fxml;
     exports edu.westga.dsdm2project1;
     exports edu.westga.dsdm2project1.View;
     opens edu.westga.dsdm2project1.View to javafx.fxml;
     exports edu.westga.dsdm2project1.Model;
-    opens edu.westga.dsdm2project1.Model to javafx.fxml;
+    opens edu.westga.dsdm2project1.Model;
 }
