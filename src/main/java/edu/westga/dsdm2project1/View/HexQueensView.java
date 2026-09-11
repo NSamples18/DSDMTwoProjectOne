d6dd7f4b4215757edabb8d49d9315618fc50d201
Justice Ricks <jr00381@my.westga.edu>
2026-09-11 11:05:37 -0400

    added a skelton to the view

--- src/main/java/edu/westga/dsdm2project1/View/HexQueensView.java
+++ src/main/java/edu/westga/dsdm2project1/View/HexQueensView.java
@@ -1,10 +1,36 @@
 package edu.westga.dsdm2project1.View;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 
 public class HexQueensView {
 
 
-    public void handleNewBoard(ActionEvent actionEvent) {
+
+    @FXML
+    private Pane boardPane;
+
+    @FXML
+    private TextField boardSizeField;
+
+    @FXML
+    private TextField cellSizeField;
+
+    @FXML
+    private Label messageLabel;
+
+    @FXML
+    private Button newBoardButton;
+
+    @FXML
+    private Label queenCountLabel;
+
+    @FXML
+    void handleNewBoard(ActionEvent event) {
+
     }
 }
--- src/test/java/edu/westga/dsdm/dsdm2project1/hexboard/TestHexBoard.java
+++ src/test/java/edu/westga/dsdm/dsdm2project1/hexboard/TestHexBoard.java
@@ -1,4 +1,5 @@
 package edu.westga.dsdm.dsdm2project1.hexboard;
 
 public class TestHexBoard {
+
 }
