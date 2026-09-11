08ec6b92aeb87daf68dd63b6ec618da6c4e90347
Justice Ricks <jr00381@my.westga.edu>
2026-09-11 11:12:08 -0400

    added a skelton to the view

--- src/main/java/edu/westga/dsdm2project1/View/HexQueensView.java
+++ src/main/java/edu/westga/dsdm2project1/View/HexQueensView.java
@@ -1,5 +1,6 @@
 package edu.westga.dsdm2project1.View;
 
+import edu.westga.dsdm2project1.ViewModel.HexBoardViewModel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -7,10 +8,12 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 
+/**
+ * View for the Hex N-Queens application.
+ */
 public class HexQueensView {
 
-
-
+    private HexBoardViewModel viewModel;
     @FXML
     private Pane boardPane;
 
@@ -29,8 +32,29 @@ public class HexQueensView {
     @FXML
     private Label queenCountLabel;
 
+    /**
+     * Initializes the view and sets up the ViewModel.
+     * Also is a handler for the new board button.
+     */
     @FXML
-    void handleNewBoard(ActionEvent event) {
+    public void handleNewBoard(ActionEvent actionEvent) {
+        try {
+            int boardSize = Integer.parseInt(this.boardSizeField.getText());
+            double cellSize = Double.parseDouble(this.cellSizeField.getText());
 
+            if (boardSize < 1 || cellSize <= 0) {
+                this.messageLabel.setText("Board size and cell size must be greater than 0.");
+                return;
+            }
+
+            this.viewModel.createBoard(boardSize);
+
+            this.queenCountLabel.setText("Queens: 0");
+            this.messageLabel.setText("");
+            this.boardPane.getChildren().clear();
+
+        } catch (NumberFormatException exception) {
+            this.messageLabel.setText("Board size and cell size must be numbers.");
+        }
     }
 }
--- src/test/java/edu/westga/dsdm/dsdm2project1/hexboard/TestHexBoard.java
+++ src/test/java/edu/westga/dsdm/dsdm2project1/hexboard/TestHexBoard.java
@@ -1,5 +1,8 @@
 package edu.westga.dsdm.dsdm2project1.hexboard;
 
+/**
+ * Test ckass for hexboard
+ */
 public class TestHexBoard {
 
 }
