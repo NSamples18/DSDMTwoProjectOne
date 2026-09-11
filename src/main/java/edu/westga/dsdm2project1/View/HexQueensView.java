b1d2323a55d7404b83a8c721e819de766d6adaa9
Justice Ricks <jr00381@my.westga.edu>
2026-09-11 12:13:49 -0400

    board is created and rendered #5

--- src/main/java/edu/westga/dsdm2project1/View/HexQueensView.java
+++ src/main/java/edu/westga/dsdm2project1/View/HexQueensView.java
@@ -1,5 +1,6 @@
 package edu.westga.dsdm2project1.View;
 
+import edu.westga.dsdm2project1.Coordinate;
 import edu.westga.dsdm2project1.ViewModel.HexBoardViewModel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -7,6 +8,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Polygon;
 
 /**
  * View for the Hex N-Queens application.
@@ -55,10 +57,63 @@ public class HexQueensView {
 
             this.queenCountLabel.setText("Queens: 0");
             this.messageLabel.setText("Created board with " + numberOfCells + " cells.");
-            this.boardPane.getChildren().clear();
+            this.renderBoard(cellSize, boardSize);
+            this.boardPane.getChildren();
 
         } catch (NumberFormatException exception) {
             this.messageLabel.setText("Board size and cell size must be numbers.");
         }
+    }
+
+    private void renderBoard(double cellSize, int boardSize) {
+        this.boardPane.getChildren().clear();
+
+        for (Coordinate coordinate : this.viewModel.getBoardCoordinates()) {
+            Polygon hexagon =
+                    this.createHexagon(coordinate, cellSize, boardSize);
+
+            this.boardPane.getChildren().add(hexagon);
+        }
+    }
+
+    private Polygon createHexagon(
+            Coordinate coordinate,
+            double cellSize,
+            int boardSize) {
+
+        Polygon hexagon = new Polygon();
+
+        double halfWidth = Math.sqrt(3) / 2 * cellSize;
+
+        hexagon.getPoints().addAll(
+                0.0, -cellSize,
+                halfWidth, -cellSize / 2,
+                halfWidth, cellSize / 2,
+                0.0, cellSize,
+                -halfWidth, cellSize / 2,
+                -halfWidth, -cellSize / 2
+        );
+
+        int radius = boardSize - 1;
+
+        double offsetX = 30 + Math.sqrt(3) * cellSize * radius;
+        double offsetY = 30 + 1.5 * cellSize * radius;
+
+        double centerX = offsetX
+                + Math.sqrt(3) * cellSize
+                * (coordinate.x() + coordinate.y() / 2.0);
+
+        double centerY = offsetY
+                + 1.5 * cellSize * coordinate.y();
+
+        hexagon.setLayoutX(centerX);
+        hexagon.setLayoutY(centerY);
+        hexagon.setStyle(
+                "-fx-fill: Orange;" +
+                        "-fx-stroke: black;" +
+                        "-fx-stroke-width: 1;"
+        );
+
+        return hexagon;
     }
 }
